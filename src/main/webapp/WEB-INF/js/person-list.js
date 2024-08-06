dda207a1c4969288c9f004237b0a4629dc0876e9
PHAM TIEN DANH <danh100121@lgcns.com>
2024-08-06 09:42:43 +0700

    update code

--- src/main/webapp/WEB-INF/js/person-list.js
+++ src/main/webapp/WEB-INF/js/person-list.js
@@ -1,4 +1,15 @@
 var timer;
+var currentQueryData = {
+  page: 0,
+  size: 12,
+  data: {
+    type: "service",
+  },
+};
+var totalPage = 0;
+let retryCount = 0;
+const maxReties = 4;
+const retryDelay = 200;
 $(".search-button").on("click", function () {
   $.ajax({
     url: "http://localhost:8080/api/users",
@@ -14,7 +25,7 @@ $(".search-button").on("click", function () {
     },
     error: function (data, status, err) {},
     complete: function () {
-      console.log("aaa");
+      console.log("error");
     },
   });
 });
@@ -56,9 +67,72 @@ function handleSearchInputChange(event) {
       },
       error: function (data, status, err) {},
       complete: function () {
-        console.log("aaa");
+        console.log("error");
       },
     });
   }, 300);
 }
 $(".search-infor input").keyup(handleSearchInputChange);
+
+function createElementPage(totalPage) {
+  const numberPage = document.querySelector(".number_page");
+  numberPage.innerHTML = "";
+  for (let i = 1; i <= totalPage; i++) {
+    let numberPageElement = document.createElement("a");
+    numberPageElement.classList.add("pageIndex");
+    numberPageElement.setAttribute("id", "pageIndex-" + (i - 1));
+    if (i == 1) {
+      numberPageElement.classList.add("active");
+    }
+    numberPageElement.textContent = i;
+    numberPageElement.onclick = function () {
+      changePage(numberPageElement);
+    };
+    numberPage.appendChild(numberPageElement);
+  }
+}
+
+// xu ly chuyen trang
+function changePage(currentPageElement) {
+  let temp = currentQueryData.page;
+  currentQueryData.page = currentPageElement.textContent - 1;
+  displayLoading();
+  $.ajax({
+    url: "http://localhost:8080/api/users",
+    method: "post",
+    data: JSON.stringify({
+      name: $(".search-button-name input").val(),
+      phone: $(".search-button-phone input").val(),
+    }),
+    contentType: "application/json",
+    success: function (response, status, xhr) {
+      searchData(response.data);
+      createElementPage(totalPage);
+      $("#total").text("Total: " + response.pagination.totalRecord);
+    },
+    error: function (data, status, err) {},
+    complete: function () {
+      console.log("error");
+    },
+  })
+    .then((response) => {
+      if (response.code == 200) {
+        const productsContainner = document.querySelector(
+          ".products_containner"
+        );
+        productsContainner.innerHTML = "";
+        const numberPageElements = document.getElementsByClassName("pageIndex");
+        Array.from(numberPageElements).forEach((elment) => {
+          elment.classList.remove("active");
+        });
+        currentPageElement.classList.add("active");
+        response.data.items.forEach((product) => {
+          createProductDetails(product);
+        });
+      } else {
+        currentQueryData.page = temp;
+        alert("Server have an error!!! Please try later!!.");
+      }
+    })
+    .finally(() => hideLoading());
+}
